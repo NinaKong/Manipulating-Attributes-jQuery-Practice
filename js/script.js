2894bbe48185682f3393a4ea803d485e1acc1dcd
NinaSleephead <kx1214@gmail.com>
2016-08-19 18:23:35 -0400

    Manipulating-Attributes-jQuery-Practice
    Practice for manipulating attributes in jQuery

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*Nina Kong*/
+
+$(document).ready(function() {
+    $(".fa-home").attr("title", "Home Icon Title");
+    $(".fa-github").attr("target", "_blank");
+    $(".fa-linkedin-square").removeAttr("href");
+    $("img").attr(
+        {
+            src: "img/imageUpdate.jpg",
+            title: "Image Update!"
+        }
+    );
+
+    var windowHeight = $(window).height();
+    var con = $(".Content");
+    con.parentsUntil($("body")).css("height", windowHeight);
+});
